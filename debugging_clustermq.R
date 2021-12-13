0b091e261afa9aa132c0a4dcd29884077919ef29
Aariq <scottericr@gmail.com>
2021-12-13 09:16:13 -0800

    still debugging

---
+++ debugging_clustermq.R
@@ -0,0 +1,10 @@
+#For debugging at the level of `clustermq` (i.e. ruling out `targets`)
+library(clustermq)
+options(
+  clustermq.scheduler = "ssh",
+  # clustermq.template = "ssh_clustermq.tmpl", #custom SSH template to use R 4.0
+  clustermq.ssh.host = "-p 2222 ericscott@hpg.rc.ufl.edu",
+  clustermq.ssh.log = "~/cmq_ssh.log" # log for easier debugging
+)
+fx = function(x) x * 2
+Q(fx, x=1:3, n_jobs=1)
