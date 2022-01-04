0f5b0aeb2ead00c186cbcfe65070a115d0536f82
Aariq <scottericr@gmail.com>
2022-01-04 13:57:09 -0500

    update to include port again.  Not all users will have hpg alias set up.

--- debugging_clustermq.R
+++ debugging_clustermq.R
@@ -3,8 +3,12 @@ library(clustermq)
 options(
   clustermq.scheduler = "ssh",
   clustermq.template = "ssh_clustermq.tmpl", #custom SSH template to use R 4.0
-  clustermq.ssh.host = "hpg",
+  clustermq.ssh.host = "-p 2222 ericscott@hpg.rc.ufl.edu",
   clustermq.ssh.log = "~/cmq_ssh.log" # log for easier debugging
 )
+# Uncomment to test if it works locally:
+# options(
+#   clustermq.scheduler = "multiprocess"
+# )
 fx = function(x) x * 2
 Q(fx, x=1:3, n_jobs=1)
