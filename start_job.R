47ff8718cdfea83705a891aae9fde9dcfc73f3e4
Aariq <scottericr@gmail.com>
2022-01-13 18:25:02 -0500

    pare down start script

--- start_job.R
+++ start_job.R
@@ -1,4 +1,4 @@
 # Source this as a local job to get the targets pipeline running the background.
 # Check progress with tar_visnetwork() or tar_progress().
-library(targets)
+
 targets::tar_make_clustermq(workers = 2L)
