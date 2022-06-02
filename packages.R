b36d626e82742914560c826e956f01da76263016
Eric R. Scott <scottericr@gmail.com>
2022-06-02 17:41:05 -0400

    update packages

--- packages.R
+++ packages.R
@@ -6,6 +6,7 @@ suppressPackageStartupMessages({
   library(tarchetypes)
   library(purrr)
   library(clustermq)
-  # library(visNetwork) #only needs to be installed locally for tar_visnetwork()
+  library(visNetwork) #only needs to be installed locally for tar_visnetwork()
+  library(rmarkdown) #only needed for rendering .Rmd files
 })
 
